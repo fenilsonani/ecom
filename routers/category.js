98cfd5e68bb2a0a7570c9bb16261e6d9e8825544
Fenil Sonani <21bmiit160@gmail.com>
2023-02-08 16:22:10 +0530

    product search

--- routers/category.js
+++ routers/category.js
@@ -15,6 +15,35 @@ router.get(`/`, async (req, res) => {
     })
 })
 
+router.get('/:id', async (req, res) => {
+    const category = await Category.findById(req.params.id).then((category) => {
+        if (category) {
+            res.status(200).json(category)
+        } else {
+            res.status(404).json({ success: false, message: "category not found!" })
+        }
+    }).catch((err) => {
+        res.status(500).json({ success: false, error: err })
+    })
+})
+
+router.put('/:id', async (req, res) => {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color
+        },
+        { new: true }
+    ).then((category) => {
+        res.status(200).json(category)
+    }).catch((err) => {
+        res.status(500).json({ success: false, error: err })
+    })
+})
+
+
 router.post(`/`, async (req, res) => {
     let category = new Category({
         name: req.body.name,
@@ -31,6 +60,18 @@ router.post(`/`, async (req, res) => {
         })
     }
     )
+})
+
+router.delete('/:id', async (req, res) => {
+    Category.findByIdAndRemove(req.params.id).then((category) => {
+        if (category) {
+            return res.status(200).json({ success: true, message: 'the category is deleted!' })
+        } else {
+            return res.status(404).json({ success: false, message: "category not found!" })
+        }
+    }).catch((err) => {
+        return res.status(400).json({ success: false, error: err })
+    })
 })
 
 module.exports = router
--- routers/product.js
+++ routers/product.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const { Category } = require('../models/category')
 const Product = require('../models/product')
 
 // api section 
@@ -16,24 +17,76 @@ router.get(`/`, (req, res) => {
 
 })
 
-
-router.post(`/`, (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-
-    product.save().then((createdProduct) => {
-        res.status(201).json(createdProduct)
+router.get(`/search`, (req, res) => {
+    const productList = Product.find().select('name image -_id').then((products) => {
+        res.status(200).json(products)
     }).catch((err) => {
         res.status(500).json({
             error: err,
             success: false
         })
-
-        // res.send(product)
     })
 })
+
+
+router.post(`/`, async (req, res) => {
+
+    const category = await Category.findById(req.body.category)
+    if (!category) return res.status(400).send('Invalid Category')
+
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        images: req.body.images,
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
+    })
+
+    product.save().then((createdProduct) => {
+        res.status(201).json(createdProduct)
+    }
+    ).catch((err) => {
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    }
+    )
+
+})
+
+router.get('/id/:id', (req, res) => {
+    const product = Product.findById(req.params.id).then((product) => {
+        if (product) {
+            res.status(200).json(product)
+        } else {
+            res.status(404).json({ success: false, message: "product not found!" })
+        }
+    }).catch((err) => {
+        res.status(500).json({ success: false, error: err })
+    })
+})
+
+router.get('/name/:name', (req, res) => {
+
+    const product = Product.findOne({ name: req.params.name }).then((product) => {
+        if (product) {
+            res.status(200).json(product)
+        } else {
+            res.status(404).json({ success: false, message: "product not found!" })
+        }
+    }).catch((err) => {
+        res.status(500).json({ success: false, error: err })
+    })
+})
+
+
 
 module.exports = router
