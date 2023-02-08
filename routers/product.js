e01dcde9dd8aa80e4022436d0caac9bd788669ff
Fenil Sonani <21bmiit160@gmail.com>
2023-02-08 16:32:18 +0530

    put added

--- routers/product.js
+++ routers/product.js
@@ -5,7 +5,7 @@ const Product = require('../models/product')
 // api section 
 router.get(`/`, (req, res) => {
 
-    const productsList = Product.find().then((products) => {
+    const productsList = Product.find().populate('category').then((products) => {
         res.status(200).json(products)
     }).catch((err) => {
         res.status(500).json({
@@ -59,8 +59,48 @@ router.post(`/`, async (req, res) => {
         })
     }
     )
-
 })
+
+router.put('/id/:id', async (req, res) => {
+
+    if (!mongoose.isValidObjectId(req.params.id)) {
+        res.status(400).send('Invalid Product Id')
+    }
+
+    const category = await Category.findById(req.body.category)
+    if (!category) return res.status(400).send('Invalid Category')
+
+
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            image: req.body.image,
+            description: req.body.description,
+            richDescription: req.body.richDescription,
+            images: req.body.images,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured,
+        },
+        { new: true }
+    ).then((product) => {
+        if (product) {
+            res.status(200).json(product)
+        } else {
+            res.status(404).json({ success: false, message: "product not found!" })
+        }
+    }).catch((err) => {
+        res.status(500).json({ success: false, error: err })
+    })
+})
+
+
+
 
 router.get('/id/:id', (req, res) => {
     const product = Product.findById(req.params.id).populate('category').then((product) => {
